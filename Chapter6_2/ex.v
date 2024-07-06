f96fdb1e40cb59ef3971d81100a8ff9c2cc25942
gaowujie <gaowujie2019@gmail.com>
2024-07-06 16:33:00 +0800

    feat(Chapter6_forward): ev 改造完成

--- Chapter6_2/ex.v
+++ Chapter6_2/ex.v
@@ -10,12 +10,23 @@ module ex (
     input wire[`RegAddrBus] waddr_i,       //目标寄存器地址
     input wire              reg_we_i,      //目标寄存器写使能
 
+    input wire[`RegBus]     hi_i,          //Hi寄存器数据
+    input wire[`RegBus]     lo_i,          //Lo寄存器数据
+
+    //输出到流水寄存器
     output reg[`RegAddrBus] waddr_o,       //目标寄存器地址
     output reg              reg_we_o,      //目标寄存器写使能
     output reg[`RegBus]     alu_res_o      //运算结果
+
+    output wire             hi_we_o,       //Hi寄存器写使能
+    output wire             lo_we_o,       //Lo寄存器写使能
+    output wire[`RegBus]    hi_o,          //指令执行阶段对Hi写入的数据
+    output wire[`RegBus]    lo_o,          //指令执行阶段对Lo写入的数据
+
 );
     reg [`RegBus] logic_res;            //保存逻辑运算结果
     reg [`RegBus] shift_res;            //保存位移运算结果
+    reg [`RegBus] move_res;             //移动操作运算结果
     wire[4:0]     shift_count = reg2_data_i[4:0];
 
     // 第一步：根据 aluop_i 运算类型，计算结果
@@ -23,6 +34,7 @@ module ex (
         if (rst == `RstEnable) begin 
             logic_res <= `ZeroWord;
             shift_res <= `ZeroWord;
+            move_res  <= `ZeroWord;
         end else begin
             case (aluop_i)
                 `ALU_OR_OP: begin
@@ -50,9 +62,21 @@ module ex (
                     // {`RegBus{reg1_data_i[`RegBus-1]}}
                     $display("sra： data1: %h, data2: %h, res: %h", reg1_data_i, shift_count, $signed(reg1_data_i) >>> shift_count);
                 end
+
+                `ALU_MOVN_OP, `ALU_MOVZ_OP: begin       // movz rd, rs, rt。 R[rd] <- R[rs]
+                    move_res <= reg1_data_i;
+                end
+                `ALU_MFHI_OP: begin                     // mfhi rd。 R[rd] <- Hi
+                    move_res <= hi_i;
+                end
+                `ALU_MFLO_OP: begin
+                    move_res <= lo_i;
+                end
+
                 `ALU_NOP_OP: begin
                     logic_res <= `ZeroWord;
                     shift_res <= `ZeroWord;
+                    move_res  <= `ZeroWord;
                 end
             endcase
         end
@@ -70,10 +94,50 @@ module ex (
             `ALU_RES_SHIFT: begin
                 alu_res_o <= shift_res;
             end
+            `ALU_RES_MOV: begin
+                alu_res_i <= move_res;
+            end
             `ALU_RES_NOP: begin
                 alu_res_o <= `ZeroWord;
             end
         endcase
+    end
+
+    // 第三步：MTLO、MTHI，需要给出 hi、lo写使能以及写入数据
+    // 此信号非传递信号，而是在该阶段产生的
+    always @(*) begin
+        if (rst == `RstEnable) begin
+            hi_we_o <= `WriteDisable;
+            hi_o    <= `ZeroWord;
+            lo_we_o <= `WriteDisable;
+            lo_o    <= `ZeroWord;
+        end else begin
+            
+            case (aluop_i)
+                `ALU_MTHI_OP: begin
+                    hi_we_o <= `WriteEnable;
+                    hi_o    <= reg1_data_i;
+
+                    lo_we_o <= `WriteDisable;
+                    lo_o    <= `ZeroWord;
+                end
+
+                `ALU_MTLO_OP: begin
+                    lo_we_o <= `WriteEnable;
+                    lo_o    <= reg1_data_i;
+
+                    hi_we_o <= `WriteDisable;
+                    hi_o    <= `ZeroWord;
+                end
+
+                default: begin
+                    hi_we_o <= `WriteDisable;
+                    hi_o    <= `ZeroWord;
+                    lo_we_o <= `WriteDisable;
+                    lo_o    <= `ZeroWord;
+                end
+            endcase
+        end
     end
 
     
