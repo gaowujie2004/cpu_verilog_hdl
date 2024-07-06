65b091e5af00683eed6472f4cd6d483196c65471
gaowujie <gaowujie2019@gmail.com>
2024-07-06 15:54:29 +0800

    feat(Chapter6_forward): add hilo 完善

--- Chapter6_2/hilo.v
+++ Chapter6_2/hilo.v
@@ -35,26 +35,60 @@ module hilo (
     end
 
     /*
-        Hi数据转发：前面指令向 Hi 写入，但还在MEM阶段，此时又要读
+        Hi数据相关：前面指令向hi写入，但还没完成，此时又要读
+        解决：数据转发（数据前推）
     */
     always @(*) begin
-        if (mem_hi_we_i==`WriteEnable) begin
-            /*
-               mthi $1         Hi    <- R[$1]   写Hi
-               mfhi $4         R[$4] <- Hi      读Hi
-               问题：当最后一条指令在 EX 阶段时，第一条指令在 MEM 阶段
-               解决（数据转发）：将第一条指令的写入Hi的数据，作为输出
-            */
-            hi_o <= mem_hi_i;
-        end else if (wb_hi_we_i == `WriteEnable) begin
-            /*
-               mthi $1         Hi    <- R[$1]   写Hi
-               nop
-               mfhi $4         R[$4] <- Hi      读Hi
-               问题：当最后一条指令在执行阶段时，nop在访存阶段，写Hi在写会阶段，但要等写回阶段结束后下一个时钟上升才能写入
-               解决（数据转发）：先输出，随后上升沿一到再写入内部的 hi 寄存器。
-            */
-            hi_o <= wb_hi_i;
+        if (rst == `RstEnable) begin
+            lo_o <= `ZeroWord;
+        end else begin
+            if (mem_hi_we_i==`WriteEnable) begin
+                /*
+                mthi $1         Hi    <- R[$1]   写Hi
+                mfhi $4         R[$4] <- Hi      读Hi
+                问题：当最后一条指令在 EX 阶段时，第一条指令在 MEM 阶段
+                解决（数据转发）：将第一条指令的写入Hi的数据，作为输出
+                */
+                hi_o <= mem_hi_i;
+            end else if (wb_hi_we_i == `WriteEnable) begin
+                /*
+                mthi $1         Hi    <- R[$1]   写Hi
+                nop
+                mfhi $4         R[$4] <- Hi      读Hi
+                问题：当最后一条指令在执行阶段时，nop在访存阶段，写Hi在写会阶段，但要等写回阶段结束后下一个时钟上升才能写入
+                解决（数据转发）：先输出，随后上升沿一到再写入内部的 hi 寄存器。
+                */
+                hi_o <= wb_hi_i;
+            end else begin
+                /*
+                mthi $1         Hi    <- R[$1]   写Hi
+                nop
+                nop
+                mfhi $4         R[$4] <- Hi      读Hi
+                说明：无数据相关问题，前面指令对hi进行写已经完成了
+                */
+                hi_o <= hi;
+            end
+        end
+    end
+
+    /*
+        Lo数据相关：前面指令向lo写入，但还没完成，此时又要读
+    */
+    always @(*) begin
+        if (rst == `RstEnable) begin
+            lo_o <= `ZeroWord;
+        end else begin
+            if (mem_lo_we_i==`WriteEnable) begin
+                //和hi一样
+                lo_o <= mem_lo_i;
+            end else if (wb_lo_we_i == `WriteEnable) begin
+                //和hi一样
+                lo <= wb_lo_i;
+            end else begin
+                //和hi一样
+                lo_o <= lo;
+            end
         end
     end
 
