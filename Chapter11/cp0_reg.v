2d984712623be4e9f0a67133b4137f2e2e7fac97
gaowujie <gaowujie2019@gmail.com>
2024-07-23 19:08:02 +0800

    refactor(11):  cp0_reg  端口更改

--- Chapter11/cp0_reg.v
+++ Chapter11/cp0_reg.v
@@ -20,31 +20,31 @@ module cp0_reg (
     
 
     output reg          timer_int_o, //定时中断触发
-    output reg[`RegBus] data_o,      //读取的值
-
-    output reg[`RegBus] count_o,     //计数器的值，只读
-    output reg[`RegBus] compare_o,   //与count_o对比的值，若相等则定时中断触发
-    output reg[`RegBus] status_o,    //控制处理器的操作模式、中断使能以及诊断状态
-    output reg[`RegBus] cause_o,     //主要记录最近⼀次异常发⽣的原因，也控制软件中断请求
-    output reg[`RegBus] epc_o,       //中断返回地址
-    output reg[`RegBus] config_o,    //处理器有关的各种配置和功能信息，只读
-    output reg[`RegBus] prid_o       //CPU硬件信息，只读
+    output reg[`RegBus] data_o       //读取的值
 );
+    reg[`RegBus] inner_count;     //计数器的值，只读
+    reg[`RegBus] inner_compare;   //与count_o对比的值，若相等则定时中断触发
+    reg[`RegBus] inner_status;    //控制处理器的操作模式、中断使能以及诊断状态
+    reg[`RegBus] inner_cause;     //主要记录最近⼀次异常发⽣的原因，也控制软件中断请求
+    reg[`RegBus] inner_epc;       //中断返回地址
+    reg[`RegBus] inner_config;    //处理器有关的各种配置和功能信息，只读
+    reg[`RegBus] inner_prid;      //CPU硬件信息，只读
+
     //先写
     always @(posedge clk) begin
         if (rst == `RstEnable) begin
             data_o    <= `ZeroWord;
-            count_o   <= `ZeroWord;
-            compare_o <= `ZeroWord;
-            status_o  <= 32'h10_00_00_00;
-            cause_o   <= `ZeroWord;
-            epc_o     <= `ZeroWord;
-            config_o  <= 32'b00000000000000001000000000000000;
-            prid_o    <= 32'b00000000010011000000000100000010;
+            inner_count   <= `ZeroWord;
+            inner_compare <= `ZeroWord;
+            inner_status  <= 32'h10_00_00_00;
+            inner_cause   <= `ZeroWord;
+            inner_epc     <= `ZeroWord;
+            inner_config  <= 32'b00000000000000001000000000000000;
+            inner_prid    <= 32'b00000000010011000000000100000010;
             timer_int_o <= `InterruptNotAssert;
         end else begin
-            count_o <= count_o + 1;
-            if (compare_o!=0 && compare_o==count_o) begin
+            inner_count <= inner_count + 1;
+            if (inner_compare!=0 && inner_compare==inner_count) begin
                 timer_int_o <= `InterruptAssert;
             end else begin
                 /*
@@ -52,28 +52,28 @@ module cp0_reg (
                  * 这个中断会⼀直保持，直到有数据被写⼊Compare寄存器。
                 */
             end
-            cause_o[15:10]  <= int_i; //Cause的第10～15bit保存外部中断声明
+            inner_cause[15:10]  <= int_i; //Cause的第10～15bit保存外部中断声明
 
             if (wb_we_i == `WriteEnable) begin
                 case (wb_waddr_i)
                     `CP0_REG_COUNT: begin
-                        count_o   <= wb_wdata_i;
+                        inner_count   <= wb_wdata_i;
                     end
                     `CP0_REG_COMPARE: begin
-                        compare_o   <= wb_wdata_i;
+                        inner_compare   <= wb_wdata_i;
                         timer_int_o <= `InterruptNotAssert;
                     end
                     `CP0_REG_STATUS: begin
-                        status_o  <= wb_wdata_i;
+                        inner_status  <= wb_wdata_i;
                     end
                     `CP0_REG_CAUSE: begin
                         //Cause寄存器只有IP[1:0]、IV、WP字段是可写的
-                        cause_o[9:8] <= wb_wdata_i[9:8];
-                        cause_o[23] <= wb_wdata_i[23];
-                        cause_o[22] <= wb_wdata_i[22];
+                        inner_cause[9:8] <= wb_wdata_i[9:8];
+                        inner_cause[23] <= wb_wdata_i[23];
+                        inner_cause[22] <= wb_wdata_i[22];
                     end
                     `CP0_REG_EPC: begin
-                        epc_o     <= wb_wdata_i;
+                        inner_epc     <= wb_wdata_i;
                     end
                 endcase
             end
@@ -104,25 +104,25 @@ module cp0_reg (
                 /* 无数据相关，正常读 */
                 case (raddr_i)
                     `CP0_REG_COUNT: begin
-                        data_o   <= count_o;
+                        data_o   <= inner_count;
                     end
                     `CP0_REG_COMPARE: begin
-                        data_o   <= compare_o;
+                        data_o   <= inner_compare;
                     end
                     `CP0_REG_STATUS: begin
-                        data_o  <= status_o;
+                        data_o  <= inner_status;
                     end
                     `CP0_REG_CAUSE: begin
-                        data_o  <= cause_o;
+                        data_o  <= inner_cause;
                     end
                     `CP0_REG_EPC: begin
-                        data_o  <= epc_o;
+                        data_o  <= inner_epc;
                     end
                     `CP0_REG_CONFIG: begin
-                        data_o  <= config_o;
+                        data_o  <= inner_config;
                     end
                     `CP0_REG_PrId: begin
-                        data_o  <= prid_o;
+                        data_o  <= inner_prid;
                     end
                 endcase
             end
@@ -134,74 +134,74 @@ module cp0_reg (
             case (exception_type_i)
                 `Exc_Interrupt: begin
                     if (is_in_delayslot_i) begin
-                        epc_o       <= inst_addr_i - 4;   //为什么呢？书中将的很清楚
-                        cause_o[31] <= `True_v;           //是否是延迟槽指令
+                        inner_epc       <= inst_addr_i - 4;   //为什么呢？书中将的很清楚
+                        inner_cause[31] <= `True_v;           //是否是延迟槽指令
                     end else begin
-                        epc_o       <= inst_addr_i;
-                        cause_o[31] <= `False_v;        
+                        inner_epc       <= inst_addr_i;
+                        inner_cause[31] <= `False_v;        
                     end
-                    cause_o[6:2] <= `ExcCode_Int;   //ExcCode field
-                    status_o[1]  <= 1'b1;           //Exc field是否异常
+                    inner_cause[6:2] <= `ExcCode_Int;   //ExcCode field
+                    inner_status[1]  <= 1'b1;           //Exc field是否异常
                 end
 
                 `Exc_Syscall: begin
-                    if (status_o[1] == 1'b0) begin
+                    if (inner_status[1] == 1'b0) begin
                         if (is_in_delayslot_i) begin
-                            epc_o       <= inst_addr_i - 4;
-                            cause_o[31] <= `True_v;
+                            inner_epc       <= inst_addr_i - 4;
+                            inner_cause[31] <= `True_v;
                         end else begin
-                            epc_o       <= inst_addr_i;
-                            cause_o[31] <= `False_v;        
+                            inner_epc       <= inst_addr_i;
+                            inner_cause[31] <= `False_v;        
                         end
                     end
-                    cause_o[6:2] <= `ExcCode_Syscall;
-                    status_o[1]  <= 1'b1;             
+                    inner_cause[6:2] <= `ExcCode_Syscall;
+                    inner_status[1]  <= 1'b1;             
                 end
 
                 `Exc_InvalidInst: begin
-                    if (status_o[1] == 1'b0) begin
+                    if (inner_status[1] == 1'b0) begin
                         if (is_in_delayslot_i) begin
-                            epc_o       <= inst_addr_i - 4;
-                            cause_o[31] <= `True_v;
+                            inner_epc       <= inst_addr_i - 4;
+                            inner_cause[31] <= `True_v;
                         end else begin
-                            epc_o       <= inst_addr_i;
-                            cause_o[31] <= `False_v;        
+                            inner_epc       <= inst_addr_i;
+                            inner_cause[31] <= `False_v;        
                         end
                     end
-                    cause_o[6:2] <= `ExcCode_InvalidInst;
-                    status_o[1]  <= 1'b1;
+                    inner_cause[6:2] <= `ExcCode_InvalidInst;
+                    inner_status[1]  <= 1'b1;
                 end
 
                 `Exc_Trap: begin
-                    if (status_o[1] == 1'b0) begin
+                    if (inner_status[1] == 1'b0) begin
                         if (is_in_delayslot_i) begin
-                            epc_o       <= inst_addr_i - 4;
-                            cause_o[31] <= `True_v;
+                            inner_epc       <= inst_addr_i - 4;
+                            inner_cause[31] <= `True_v;
                         end else begin
-                            epc_o       <= inst_addr_i;
-                            cause_o[31] <= `False_v;        
+                            inner_epc       <= inst_addr_i;
+                            inner_cause[31] <= `False_v;        
                         end
                     end
-                    cause_o[6:2] <= `ExcCode_Trap;
-                    status_o[1]  <= 1'b1;
+                    inner_cause[6:2] <= `ExcCode_Trap;
+                    inner_status[1]  <= 1'b1;
                 end
 
                 `Exc_Overflow: begin
-                    if (status_o[1] == 1'b0) begin
+                    if (inner_status[1] == 1'b0) begin
                         if (is_in_delayslot_i) begin
-                            epc_o       <= inst_addr_i - 4;
-                            cause_o[31] <= `True_v;
+                            inner_epc       <= inst_addr_i - 4;
+                            inner_cause[31] <= `True_v;
                         end else begin
-                            epc_o       <= inst_addr_i;
-                            cause_o[31] <= `False_v;        
+                            inner_epc       <= inst_addr_i;
+                            inner_cause[31] <= `False_v;        
                         end
                     end
-                    cause_o[6:2] <= `ExcCode_Overflow;
-                    status_o[1]  <= 1'b1;
+                    inner_cause[6:2] <= `ExcCode_Overflow;
+                    inner_status[1]  <= 1'b1;
                 end
 
                 `Exc_Eret: begin
-                    status_o[1]   <=  1'b0;
+                    inner_status[1]   <=  1'b0;
                 end
             endcase
         end
